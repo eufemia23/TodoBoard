540d2c0ea65bbaafbe68758db1004e1a89361150
Aurelia <aurelia.ao.04@gmail.com>
2025-12-05 12:49:58 +0100

    added to do/done task distinction on the frontend

--- frontend/src/pages/TasksPage.jsx
+++ frontend/src/pages/TasksPage.jsx
@@ -1,18 +1,74 @@
-import { Checkbox, Label } from "@heroui/react";
 import Task from "../components/Task";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Tabs } from "@heroui/react";
+import { PlusIcon } from "lucide-react";
 
 const TasksPage = () => {
-  
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/todos");
+        setTodos(res.data);
+      } catch (error) {
+        console.log("Error fetching notes", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchTodos();
+  }, []);
 
   return (
     <div className="flex justify-center mt-10 ">
-      <div className="bg-white w-2/3 max-w-150 min-h-150 rounded-lg shadow-lg px-10 py-5">
+      <div className="bg-white w-2/3 max-w-150 min-h-150 rounded-lg shadow-lg px-10 py-5 animate-appear">
         <div className="text-[20px] mb-5 font-title">My Tasks</div>
 
+        <Tabs className="w-full max-w-md font-default">
+          <Tabs.ListContainer>
+            <Tabs.List aria-label="Options">
+              <Tabs.Tab id="overview">
+                To Do
+                <Tabs.Indicator />
+              </Tabs.Tab>
+
+              <Tabs.Tab id="reports">
+                Done
+                <Tabs.Indicator />
+              </Tabs.Tab>
+            </Tabs.List>
+          </Tabs.ListContainer>
+          <Tabs.Panel className="pt-4" id="overview">
+            <p>View your project overview and recent activity.</p>
+          </Tabs.Panel>
+          <Tabs.Panel className="pt-4" id="analytics">
+            <p>Track your metrics and analyze performance data.</p>
+          </Tabs.Panel>
+          <Tabs.Panel className="pt-4" id="reports">
+            <p>Generate and download detailed reports.</p>
+          </Tabs.Panel>
+        </Tabs>
+
         <div>
-          <Task task={"read the first page of the book"} />
-          <Task task={"write an essay about the effect of painful on one's mental health and decide which stand i wanna take: is it good or bad?"} />
-          <Task task={"hello"}/>
+          {isLoading && <div>Loading notes...</div>}
+
+          {todos.map((todo) => (
+            <Task key={todo._id} task={todo.content} />
+          ))}
+        </div>
+
+        <div className="flex w-max mx-auto mt-7">
+          <button className="text-[15px] font-default flex hover:bg-indigo-800/30 hover:cursor-pointer transition duration-300 ease-smooth pt-2 pb-2 pr-5 pl-4 rounded-full hover:scale-110 bg-gray-200">
+            <PlusIcon
+              absoluteStrokeWidth={true}
+              strokeWidth={1.6}
+              className="size-6 pr-2 -mt-px"
+            />
+            <div className="">Add Task</div>
+          </button>
         </div>
       </div>
     </div>
